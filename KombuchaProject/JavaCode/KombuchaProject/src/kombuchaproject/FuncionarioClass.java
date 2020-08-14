712685820362499e4c0cb850e18ad40759e19cef
Deive <deive.leal@gmail.com>
2020-08-14 14:29:59 -0300

    Inicio do projeto

---
+++ KombuchaProject/JavaCode/KombuchaProject/src/kombuchaproject/FuncionarioClass.java
@@ -0,0 +1,36 @@
+package kombuchaproject;
+/**
+ *
+ * @author deive
+ */
+
+public class FuncionarioClass {
+    private int idFuncionario;
+    private String nomeFuncionario;
+    private String cargo;
+
+    public int getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(int idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+        
+}
---
+++ KombuchaProject/JavaCode/KombuchaProject/src/kombuchaproject/KombuchaProject.java
@@ -0,0 +1,10 @@
+
+package kombuchaproject;
+
+public class KombuchaProject {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+    
+}
