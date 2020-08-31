279622c5822f64a552632731e900d8c1dd813edc
Carolina Zanotto <zanotto.carol@gmail.com>
2020-08-31 18:14:21 -0300

    adição dos pacotes dao e conexao

---
+++ KombuchaProject/JavaCode/KombuchaProject/src/conexao/FabricaDeConexao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexao;
+
+/**
+ *
+ * @author Carolina
+ */
+public class FabricaDeConexao {
+    
+    private final String DRIVE = "com.mysql.jdbc.Driver";
+    private final String URL = "jdbc:mysql://localhost:3306/tribos_hombucha";
+    private final String USER = "root";
+    private final String PASS = "013130";
+    
+    public Connection getConnetion(){
+    }
+    
+    
+}
