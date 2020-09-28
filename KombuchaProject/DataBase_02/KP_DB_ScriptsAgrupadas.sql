4bf9f72dc2d5cf613f3950a8eac4d7a9196a620d
Carolina Zanotto <zanotto.carol@gmail.com>
2020-09-28 03:19:35 -0300

    BD procedure de alteração de estado de fermentador - BIG

--- KombuchaProject/DataBase_02/KP_DB_ScriptsAgrupadas.sql
+++ KombuchaProject/DataBase_02/KP_DB_ScriptsAgrupadas.sql
@@ -2,8 +2,8 @@
 CREATE SCHEMA tribos_kombucha;
 USE  tribos_kombucha;
 
--- Criação das tabelas
-
+-- 1.0 - Criação das tabelas - [9 tabelas]
+-- 1.1
 CREATE TABLE Funcionario(
 	idFuncionario VARCHAR(11) PRIMARY KEY,
     nomeFuncionario CHAR(100) NOT NULL,
@@ -11,11 +11,13 @@ CREATE TABLE Funcionario(
     senha VARCHAR(20)
 );
 
+-- 1.2
 CREATE TABLE ItemDeEstoque (
 	idItem INT PRIMARY KEY,
     tipoItem VARCHAR(50) NOT NULL
 );
 
+-- 1.3
 CREATE TABLE Estoque(
 	idItem INT,
 	idItemEstoque INT,
@@ -26,6 +28,7 @@ CREATE TABLE Estoque(
     PRIMARY KEY(idItem , idItemEstoque)
 );
 
+-- 1.4
 CREATE TABLE Insumo(
     idInsumo INT PRIMARY KEY,
     idItemEstoque INT,
@@ -35,16 +38,18 @@ CREATE TABLE Insumo(
     FOREIGN KEY (idItemEstoque) references ItemDeEstoque(idItem)
 );
 
+-- 1.5
 CREATE TABLE Fermentador(
     idFermentador INT PRIMARY KEY,
     idItemEstoque INT,
     estadoProducao BOOL,
     volume DOUBLE,
+    dataUtilizacao DATE,
     
     FOREIGN KEY (idItemEstoque) references ItemDeEstoque(idItem)
 );
 
-
+-- 1.6
 CREATE TABLE EmbalagemKombucha(
     idEmbalagem INT PRIMARY KEY,
     idItemEstoque INT,
@@ -54,8 +59,9 @@ CREATE TABLE EmbalagemKombucha(
     FOREIGN KEY (idItemEstoque) references ItemDeEstoque(idItem)
 );
 
+-- 1.7
 CREATE TABLE SaborKombucha(
-    idKombucha  INT AUTO_INCREMENT PRIMARY KEY,
+    idKombucha  INT PRIMARY KEY,
     nomeKombucha VARCHAR(100) NOT NULL,
     chaBase VARCHAR(100) NOT NULL,
     idChaBase INT,
@@ -68,6 +74,7 @@ CREATE TABLE SaborKombucha(
     FOREIGN KEY (idChaBase) references Insumo(idInsumo)
 );
 
+-- 1.8
 CREATE TABLE Pedido(
     idPedido INT AUTO_INCREMENT PRIMARY KEY,
     idSabor INT,
@@ -81,6 +88,7 @@ CREATE TABLE Pedido(
     quantidadeAcucar DOUBLE,
     quantidadeEmbalagem  INT,
     dataEntradaPedido DATETIME,
+    estadoPedido ENUM ('em execução', 'finalizado'),
     
     FOREIGN KEY (idSabor) REFERENCES SaborKombucha(idKombucha),
     FOREIGN KEY (idFermentador) REFERENCES Fermentador(idFermentador),
@@ -88,31 +96,29 @@ CREATE TABLE Pedido(
     FOREIGN KEY (idFuncionario) references Funcionario(idFuncionario)
 );
 
-
+-- 1.9
 CREATE TABLE KombuchaPronto(
     idKombuchaPronto INT PRIMARY KEY ,
     idItemEstoque INT,
     idSabor INT,
-	nomeProduto VARCHAR(50),
-    dataFabricacao DATE,
+	nomeProduto VARCHAR(100),
 
     FOREIGN KEY (idItemEstoque) references ItemDeEstoque(idItem),
     FOREIGN KEY (idSabor) references SaborKombucha (idKombucha)
 );
 
--- População inicial do banco
-
+-- 2.0 - População inicial do banco - [9 inserções]
+-- 2.1
 INSERT INTO Funcionario(
     idFuncionario,
     nomeFuncionario,
     cargo, 
     senha)
 VALUES
-
     (1, 'Daniel', 'Teste', '1111'),
     ('06146045675', 'Deive', 'Dev', '123');
 
-    
+ -- 2.2   
 INSERT INTO ItemDeEstoque(
     idItem,
     tipoItem)
@@ -122,7 +128,7 @@ VALUES
     (3, 'Fermentador'),
     (4, 'Kombucha Pronto');
     
-
+-- 2.3
 INSERT INTO Insumo(
     idInsumo,
     idItemEstoque,
@@ -137,23 +143,25 @@ VALUES
 	(6, 1, 'Extrato de Hibisco-Uva', 20),
 	(7, 1, 'Extrato de Capim-Santo Gengibre-Limão', 20);
  
+ -- 2.4
  INSERT INTO Fermentador(
 	idFermentador,
     idItemEstoque,
     estadoProducao,
-    volume)
+    volume,
+    dataUtilizacao)
 VALUES
-	(0, 3, true, 4),
-    (1, 3, false, 4.0),
-    (2, 3, false, 4.0),
-    (3, 3, false, 4.0),
-    (4, 3, false, 4.0),
-    (5, 3, false, 4.0),
-    (6, 3, false, 4.0),
-    (7, 3, false, 4.0),
-    (8, 3, false, 4.0);
+	(0, 3, true, 4, '2020-09-25'),
+    (1, 3, false, 4.0, '2020-09-25'),
+    (2, 3, false, 4.0,'2020-09-25'),
+    (3, 3, false, 4.0, '2020-09-25'),
+    (4, 3, false, 4.0, '2020-09-25'),
+    (5, 3, false, 4.0, '2020-09-25'),
+    (6, 3, false, 4.0, '2020-09-25'),
+    (7, 3, false, 4.0, '2020-09-25'),
+    (8, 3, false, 4.0,'2020-09-25');
 
-
+-- 2.5
  INSERT INTO EmbalagemKombucha(
         idEmbalagem,
         idItemEstoque,
@@ -165,6 +173,7 @@ VALUES
     (3, 2, 'Verde', 'Embalagem Hibisco'),
     (4, 2, 'Laranja', 'Embalagem Gengibre');
      
+-- 2.6
  INSERT INTO Estoque(
     idItem,
 	idItemEstoque,
@@ -182,11 +191,13 @@ VALUES
         (2, 2, 'Embalagem Abacaxi', 32),
         (3, 2, 'Embalagem Hibisco', 32),
         (4, 2, 'Embalagem Gengibre', 32),
-        (1, 4, 'Kombucha Pronto Anis', 48),
-        (2, 4, 'Kombucha Pronto Abacaxi', 48),
-        (3, 4, 'Kombucha Pronto Hibisco', 48),
-        (4, 4, 'Kombucha Pronto Gengibre', 48);
-
+        (1, 4, 'Kombucha Pronto Anis', 12),
+        (2, 4, 'Kombucha Pronto Abacaxi', 12),
+        (3, 4, 'Kombucha Pronto Hibisco', 12),
+        (4, 4, 'Kombucha Pronto Gengibre', 12),
+        (1, 3, 'Fermentador', 8);
+        
+-- 2.7
  INSERT INTO SaborKombucha(
     idKombucha,
 	nomeKombucha,
@@ -202,10 +213,19 @@ VALUES
         (3, 'Hibisco-Uva', 'Extrato de Hibisco-Uva', 6, 0.025, 0.3, 0.045, 1),
         (4, 'Capim-Santo Gengibre-Limão', 'Extrato de Capim-Santo Gengibre-Limão', 7, 0.025, 0.3, 0.045, 1);
         
-        
+-- 2.8
+INSERT INTO KombuchaPronto( 
+	idKombuchaPronto,
+    idItemEstoque,
+    idSabor,
+    nomeProduto)
+	VALUES 
+		( 1, 4, 1, 'Kombucha Anis'),
+        ( 2, 4, 1, 'Kombucha Abacaxi com Hortelã'),
+        ( 3, 4, 1, 'Kombucha Hibisco-Uva'),
+        ( 4, 4, 1, 'Kombucha Anis Capim-Santo Gengibre-Limão');
 
-
-
+-- 2.9
 INSERT INTO Pedido( idSabor, 
 					nomeSabor,
                     quantidadeProducao, 
@@ -216,38 +236,76 @@ INSERT INTO Pedido( idSabor,
                     quantidadeAgua,
                     quantidadeAcucar,
                     quantidadeEmbalagem,
-                    dataEntradaPedido)
-	VALUES ( 1, 'teste', 1, 0, NULL, 1, 1, 1, 1, 1, NOW());
-    
--- Criação de Usuarios e Controle de Acesso
+                    dataEntradaPedido,
+                    estadoPedido)
+	VALUES 
+		( 1, 'teste', 1, 1, NULL, 1, 1, 1, 1, 1, NOW(), 'finalizado'),
+		( 1, 'teste2', 1, 1, NULL, 1, 1, 1, 1, 1, NOW(), 'em execução');
 
+
+-- 3.0 - Views - [4 views]
+-- 3.1
+CREATE VIEW fermentadorAlocadoPedido AS
+	SELECT  f.idFermentador 'idFermentador',
+			f.dataUtilizacao 'DataUltimoUso',
+			p.idPedido 'idPedido',
+			p.estadoPedido 'EstadoPedido'
+			FROM fermentador f 
+			inner join pedido p ON f.idFermentador = p.idFermentador;
+
+-- 3.2
+CREATE VIEW ingredientes_saboreskombucha AS
+	SELECT * FROM saborkombucha;
+
+-- 3.3
+CREATE VIEW qtd_base_sabor AS 
+    SELECT     chaBase,
+            prePreparoQuantCha,
+            prePreparoQuantAgua
+    FROM saborkombucha;
+
+-- 3.4
+CREATE VIEW estoque_ordenado AS
+	SELECT * FROM Estoque
+		ORDER BY idItemEstoque, idItem;
+
+
+-- 4.0 - Criação de Usuarios e Controle de Acesso - [7 itens]
+-- 4.1
 CREATE USER IF NOT EXISTS 'desenvolvedor'@'localhost'
 	IDENTIFIED BY 'dev12345'
 	PASSWORD EXPIRE INTERVAL 90 DAY;
-    
+
+-- 4.2
 CREATE USER IF NOT EXISTS 'admdb'@'localhost'
 	IDENTIFIED BY 'mandb123'
     PASSWORD EXPIRE INTERVAL 365 DAY;
-    
+
+-- 4.3
 CREATE USER IF NOT EXISTS 'userdb'@'localhost'
 	IDENTIFIED BY 'user1234'
     PASSWORD EXPIRE INTERVAL 365 DAY;
-    
+  
+-- 4.4
 GRANT ALTER, DELETE, INSERT, SELECT, UPDATE
 	ON tribos_kombucha.*
     TO 'desenvolvedor'@'localhost' WITH GRANT OPTION;
-    
+
+-- 4.5
 GRANT ALL
 	ON tribos_kombucha.*
     TO 'admdb'@'localhost' WITH GRANT OPTION;
-    
+
+-- 4.6
 GRANT DELETE, INSERT, SELECT, UPDATE
 	ON tribos_kombucha.*
     TO 'userdb'@'localhost';
-    
+
+-- 4.7
 FLUSH PRIVILEGES;
-    
--- Função de Agregação
+
+-- 5.0 - Função de Agregação - [13 itens]
+-- 5.1
 DROP PROCEDURE IF EXISTS NumeroPedidoPorData;
 DELIMITER $$
 CREATE PROCEDURE NumeroPedidoPorData(IN dataDeEntradaIN DATE)
@@ -260,7 +318,7 @@ DELIMITER ;
 
  
  -- SUB PROCEDURES MONTAGEM DE PEDIDOS
- 
+ -- 5.2
  -- Monta chá para pedido
 DROP FUNCTION IF EXISTS montagemCha;
 DELIMITER $$
@@ -278,6 +336,7 @@ END;
 $$
 DELIMITER ;
 
+-- 5.3
 -- Monta água para pedido
 DROP FUNCTION IF EXISTS montagemAgua;
 DELIMITER $$
@@ -294,11 +353,8 @@ RETURN quantidadeDeProducao;
 END;
 $$
 DELIMITER ;
-/* 
-SELECT prePreparoQuantCha INTO prePreparoQuantCha_aux FROM tribos_kombucha.SaborKombucha WHERE idKombucha = idKombucha;
-        SET quantidadeDeProducao = prePreparoQuantCha_aux * quantidadeDeProducao;
-*/
 
+-- 5.4
 -- Monta numero de embalagens para pedido
 DROP FUNCTION IF EXISTS montagemEmbalagem;
 DELIMITER $$
@@ -314,6 +370,7 @@ END;
 $$
 DELIMITER ;
 
+-- 5.5
 -- Monta quantidade de acucar por pedido
 DROP FUNCTION IF EXISTS montagemAcucar;
 DELIMITER $$
@@ -333,6 +390,7 @@ END;
 $$
 DELIMITER ;
 
+-- 5.6
 -- Seta fermentador para pedido
 DROP PROCEDURE IF EXISTS escolheFermentador;
 DELIMITER $$
@@ -344,56 +402,96 @@ BEGIN
     DECLARE volumeProducao_aux DOUBLE;
 	
     IF volumeProducao <= 4.0 THEN
- 
-      SELECT idFermentador INTO idFermentador_aux FROM tribos_kombucha.fermentador
-		WHERE estadoProducao = 0
-        ORDER BY idFermentador LIMIT 1;
+		SELECT idFermentador INTO idFermentador_aux FROM tribos_kombucha.fermentador
+			WHERE estadoProducao = 0
+			ORDER BY idFermentador LIMIT 1;
 		CALL alteraEstadoFermentador(idFermentador_aux);
         UPDATE pedido SET idFermentador = idFermentador_aux WHERE idPedido = idPedido_aux;
-        /*
-        INSERT INTO pedido(idFermentador) 
-				VALUES(idFermentador_aux)
-                WHERE idPedido = idPedido_aux;
-        */
-   ELSEIF volumeProducao > 4.0 AND volumeProducao < 8.0 THEN
-		-- SET volumeProducao_aux = volumeProducao - 4;
+      
+	ELSEIF volumeProducao > 4.0 AND volumeProducao < 8.0 THEN
 		SELECT idFermentador INTO idFermentador_aux FROM tribos_kombucha.fermentador
 			WHERE estadoProducao = 0
             ORDER BY idFermentador LIMIT 1;
-            CALL alteraEstadoFermentador(idFermentador_aux);
-            UPDATE pedido SET idFermentador = idFermentador_aux WHERE idPedido = idPedido_aux;
-            /*
-            INSERT INTO pedido( idFermentador)
-				VALUES(idFermentador_aux);
-             */   
+		CALL alteraEstadoFermentador(idFermentador_aux);
+		UPDATE pedido SET idFermentador = idFermentador_aux WHERE idPedido = idPedido_aux;
+            
 		SELECT idFermentador INTO idFermentador_aux2 FROM tribos_kombucha.fermentador
 			WHERE estadoProducao = 0
             ORDER BY idFermentador LIMIT 1;
-            CALL alteraEstadoFermentador(idFermentador_aux2);
-            UPDATE pedido SET idFermentadorExtra = idFermentador_aux2 WHERE idPedido = idPedido_aux;
-            /*
-            INSERT INTO pedido( idFermentador, idFermentadorExtra)
-				VALUES(idFermentador_aux, idFermentador_aux2);
-			*/
+		CALL alteraEstadoFermentador(idFermentador_aux2);
+		UPDATE pedido SET idFermentadorExtra = idFermentador_aux2 WHERE idPedido = idPedido_aux;
 	END IF;
-   
 END;
 $$
 DELIMITER ;
 
--- 
 
+-- 5.7
+-- Altera estado(ocupado) e ultima utilização do fermentador
 DROP PROCEDURE IF EXISTS alteraEstadoFermentador;
 DELIMITER $$
 	CREATE PROCEDURE alteraEstadoFermentador (IN idFermentador_aux INT)
 	BEGIN 
         UPDATE fermentador SET estadoProducao = 1 WHERE idFermentador = idFermentador_aux ;
+        UPDATE fermentador SET dataUtilizacao = now() WHERE idFermentador = idFermentador_aux ;
 	END;
 $$
 DELIMITER ;
 
--- 
+-- 5.8
+-- Muda estado(livre) do fermentador
+DROP PROCEDURE IF EXISTS desocupaFermentador;
+DELIMITER $$
+	CREATE PROCEDURE desocupaFermentador(IN idFermentador_aux INT)
+	BEGIN 
+        UPDATE fermentador SET estadoProducao = 0 WHERE idFermentador = idFermentador_aux ;
+	END;
+$$
+DELIMITER ;
 
+-- 5.9
+-- Finaliza pedido e chama procedures de incremento de Kombucha Pronta e desocupa o fermentador
+DROP PROCEDURE IF EXISTS alteraEstadoPedido;
+DELIMITER $$
+	CREATE PROCEDURE alteraEstadoPedido(IN idPedidoIN INT)
+	BEGIN 
+		DECLARE idFermentador_aux INT;
+        
+        SELECT idFermentador INTO idFermentador_aux FROM Pedido
+			WHERE idPedido = idPedidoIN;
+    
+        UPDATE pedido SET estadoPedido = 'finalizado' WHERE idPedido = idPedidoIN ;
+        
+        CALL transformaPedidoKombuchaPronta(idPedidoIN);
+        CALL desocupaFermentador(idFermentador_aux);
+	END;
+$$
+DELIMITER ;
+
+-- 5.10
+-- Pega quantidade produzida no pedido e incrementa o estoque
+DROP PROCEDURE IF EXISTS transformaPedidoKombuchaPronta;
+DELIMITER $$
+	CREATE PROCEDURE transformaPedidoKombuchaPronta(IN idPedidoIN INT)
+	BEGIN 
+		DECLARE quantidadeProduzida_aux INT;
+        DECLARE idSabor_aux INT;
+        
+        SELECT quantidadeProducao INTO quantidadeProduzida_aux FROM Pedido
+			WHERE idPedido = idPedidoIN;
+            
+		SELECT idSabor INTO idSabor_aux FROM Pedido
+			WHERE idPedido = idPedidoIN;
+        
+        UPDATE Estoque SET quantItem = quantItem + quantidadeProduzida_aux 
+			WHERE idItem = idSabor_aux AND idItemEstoque = 4 ;
+        
+	END;
+$$
+DELIMITER ;
+
+-- 5.11
+-- Incrementa insumo dentro do estoque
 DROP PROCEDURE IF EXISTS reposicaoEstoque;
 DELIMITER $$
 CREATE PROCEDURE reposicaoEstoque(IN idItemIN INT, IN idItemEstoqueIN INT, IN quantIncrementoIN DOUBLE) 
@@ -415,12 +513,13 @@ CREATE PROCEDURE reposicaoEstoque(IN idItemIN INT, IN idItemEstoqueIN INT, IN qu
 $$
 DELIMITER ;
 
-
+-- 5.12
+-- Procedure que automatiza a montagem dos pedidos
 DROP PROCEDURE IF EXISTS montaPedidoPreparo;
 DELIMITER $$
 
 	CREATE PROCEDURE montaPedidoPreparo (IN idFuncionario VARCHAR(11),
-                                         in saborKombuchaIN VARCHAR(52), -- nova entrada
+                                         in saborKombuchaIN VARCHAR(52), 
 										 IN quantidadeDeProducao DOUBLE)
 									 
 	BEGIN 
@@ -438,11 +537,13 @@ DELIMITER $$
         
         
 		SELECT idKombucha INTO idSabor_aux FROM SaborKombucha 
-			WHERE nomeKombucha = saborKombuchaIN; -- novo select     
+			WHERE nomeKombucha = saborKombuchaIN;  
 
 		SELECT NOW() INTO dataEntradaPedido;
         
-        -- FUNÇÃO DE DECREMENTO DE SKOOBY
+        -- decrementa scoky
+        UPDATE Estoque SET quantItem = quantItem - 1 
+			WHERE idItem = 3 AND idItemEstoque = 1;
 	
         SELECT quantidadeDeProducao INTO quantidadeDeProducao_aux;
         SET prePreparoQuantCha_aux = montagemCha(idSabor_aux, quantidadeDeProducao);
@@ -460,8 +561,7 @@ DELIMITER $$
 		SET idPedido_aux = idPedido_aux + 1;
 		
         
-        INSERT INTO Pedido( 
-							idSabor,
+        INSERT INTO Pedido( idSabor,
                             nomeSabor,
                             quantidadeProducao,
                             idFermentador,
@@ -470,7 +570,8 @@ DELIMITER $$
                             quantidadeAgua,
                             quantidadeAcucar,
                             quantidadeEmbalagem,
-                            dataEntradaPedido)
+                            dataEntradaPedido,
+                            estadoPedido)
 		VALUES(
 			idSabor_aux,
             saborKombuchaIN,
@@ -481,12 +582,12 @@ DELIMITER $$
             prePreparoQuantAgua_aux,
             quantidadeAcucar_aux,
             quantEmbalagem,
-            dataEntradaPedido
+            dataEntradaPedido,
+            'em execução'
 			);
             
-            CALL escolheFermentador(idPedido_aux, volumeProducao);
-            
             -- inserção dos fermentadores no pedido
+            CALL escolheFermentador(idPedido_aux, volumeProducao);     
 	END;
 $$
 DELIMITER ;
@@ -497,6 +598,7 @@ DELIMITER ;
 SET @quantidadeDeProducao = 3;
 CALL montaPedidoPreparo('1', 'Anis', @quantidadeDeProducao);
 
+SELECT * FROM FERMENTADOR;
 SELECT @quantidadeDeProducao;
 
 
@@ -505,6 +607,7 @@ SELECT * FROM pedido;
 SELECT * FROM fermentador;
 */
 
+-- 5.13
 -- Procedure dar baixa no estoque
 DROP PROCEDURE IF EXISTS darBaixaPedido;
 DELIMITER $$
@@ -561,7 +664,8 @@ SELECT * FROM itemdeestoque;
 SELECT * FROM estoque;
 */
 
--- TRIGGER 
+-- 6.0 - Trigger - [13 itens]
+-- 6.1
 DROP TRIGGER IF EXISTS darBaixaEstoque;
 DELIMITER $$
 CREATE TRIGGER darBaixaEstoque
@@ -578,11 +682,45 @@ FOR EACH ROW
 $$
 DELIMITER ;
 
--- VIEWS
--- SELECT * FROM saborkombucha
-
-/*
-FALTA IMPLEMENTAR:
-	- VIEWS
-    - BACKUP    
-*/
+-- 6.0 - Cursor - [2 itens]
+-- 6.1
+DROP PROCEDURE IF EXISTS estoqueEmBaixa_cursor;
+DELIMITER $$
+	CREATE PROCEDURE estoqueEmBaixa_cursor()
+	BEGIN 
+        DECLARE fim INT DEFAULT FALSE;
+        DECLARE itemEmEstoque VARCHAR(100);
+        DECLARE quantidadeEmEstoque DOUBLE;
+        
+        
+        DECLARE blocoCursor CURSOR FOR
+			SELECT nomeItem, quantItem FROM Estoque
+				WHERE quantItem < 21 OR quantItem IS NULL;
+		
+        DECLARE CONTINUE handler 
+		FOR NOT found 
+			SET fim = TRUE; 
+        
+        CREATE temporary TABLE itensBaixaQuant( 
+			itemEmEstoque VARCHAR(100),
+			quantidadeEmEstoque DOUBLE
+		);
+        
+        OPEN blocoCursor;
+        READ_LOOP:
+        LOOP
+			FETCH blocoCursor INTO itemEmEstoque, quantidadeEmEstoque;
+            INSERT INTO itensBaixaQuant	
+				VALUES (itemEmEstoque, quantidadeEmEstoque);
+			IF fim THEN
+				LEAVE READ_LOOP;
+			END IF;
+		END LOOP;
+        CLOSE blocoCursor;
+                       
+        SELECT * FROM  itensBaixaQuant;
+    
+    DROP temporary TABLE itensBaixaQuant;
+	END;
+$$
+DELIMITER ;
